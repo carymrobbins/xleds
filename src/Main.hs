e885a04c59aa0d0e66f26f76c75bb1432cf13456
Cary Robbins <carymrobbins@gmail.com>
2017-06-16 02:34:59 -0500

    Work with both screen and keyboard backlight

--- src/Main.hs
+++ src/Main.hs
@@ -1,27 +1,124 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase #-}
-module Main where
+{-# LANGUAGE RecordWildCards #-}
+module Main (main) where
 
 import Data.List
 import Data.Maybe
+import Data.Monoid
+import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
 import Text.Read
 
 usage = intercalate "\n"
-  [ "Usage: xleds [options]"
+  [ "Usage: xleds [key|screen] [command]"
   , ""
-  , "Options:"
+  , "Commands:"
   , "  -get"
-  , "  -set <percentage> or = <percentage>"
-  , "  -inc <percentage> or + <percentage>"
-  , "  -dec <percentage> or - <percentage>"
+  , "  -set <amount|percentage%> or = <amount|percentage%>"
+  , "  -inc <amount|percentage%> or + <amount|percentage%>"
+  , "  -dec <amount|percentage%> or - <amount|percentage%>"
   ]
 
-ledsDir = "/sys/class/leds/smc::kbd_backlight"
-ledsBrightnessFile = ledsDir ++ "/brightness"
-ledsMaxBrightnessFile = ledsDir ++ "/max_brightness"
+data Args = Args
+  { argDevice :: Device
+  , argCommand :: Command
+  }
+
+data Device = Keyboard | Screen deriving Show
+
+data Command
+  = Get
+  | Set Amount
+  | Inc Amount
+  | Dec Amount
+
+data Amount
+  = RawAmount Int
+  | PercentAmount Percent
+
+data ArgFailure = ShowHelp | InvalidUsage String
+
+data ParseResult e a = ParseError e | ParseSuccess a
+
+instance Functor (ParseResult e) where
+  fmap f p = case p of
+    ParseError e -> ParseError e
+    ParseSuccess a -> ParseSuccess $ f a
+
+instance Applicative (ParseResult e) where
+  pure = ParseSuccess
+  mf <*> ma = case (mf, ma) of
+    (ParseSuccess f, ParseSuccess a) -> ParseSuccess $ f a
+    (ParseError e, _) -> ParseError e
+    (_, ParseError e) -> ParseError e
+
+instance Monad (ParseResult e) where
+  ma >>= f = case ma of
+    ParseError e -> ParseError e
+    ParseSuccess a -> f a
+
+parseArgs :: [String] -> ParseResult ArgFailure Args
+parseArgs args = case args of
+  [] -> ParseError $ InvalidUsage "Missing argument"
+  [help] | help `elem` ["-h", "--help"] -> ParseError ShowHelp
+  device : command -> Args <$> parseDevice device <*> parseCommand command
+  where
+  parseDevice = \case
+    "key" -> ParseSuccess Keyboard
+    "screen" -> ParseSuccess Screen
+    other -> ParseError $ InvalidUsage $ "Expected key or screen, got: " <> other
+
+  parseCommand = \case
+    ["-get"] -> ParseSuccess Get
+    [set, val] | set `elem` ["-set", "="] -> Set <$> parseAmount val
+    [inc, val] | inc `elem` ["-inc", "+"] -> Inc <$> parseAmount val
+    [dec, val] | dec `elem` ["-dec", "-"] -> Dec <$> parseAmount val
+    _ -> ParseError $ InvalidUsage "Invalid command"
+
+  parseAmount s = case s of
+    "" -> ParseError $ InvalidUsage "Empty amount value"
+    _ -> case readMaybe s of
+      Just n -> ParseSuccess $ RawAmount n
+      Nothing -> case readMaybe $ init s of
+        Just p | p >= 0 && p <= 100 -> ParseSuccess $ PercentAmount $ Percent $ p / 100
+        Nothing -> ParseError $ InvalidUsage $ "Invalid amount value: " <> s
+
+findM :: Monad m => (a -> m Bool) -> [a] -> m (Maybe a)
+findM f xs = case xs of
+  [] -> return Nothing
+  y:ys -> do
+    b <- f y
+    if b then return (Just y) else findM f ys
+
+getLedsDir :: Device -> IO String
+getLedsDir device = case device of
+  Keyboard -> findDir keyboardDirs
+  Screen -> findDir screenDirs
+  where
+  findDir dirs = findM doesDirectoryExist dirs >>= \case
+    Just d -> return d
+    Nothing -> do
+      hPutStrLn stderr $ "Could not locate backlight for device: " <> show device
+      exitFailure
+
+  keyboardDirs =
+    [ "/sys/class/leds/smc::kbd_backlight"
+    , "/sys/class/leds/dell::kbd_backlight"
+    ]
+
+  screenDirs =
+    [ "/sys/class/backlight/intel_backlight"
+    ]
+
+getLedsBrightnessFile :: FilePath -> String
+getLedsBrightnessFile = (++ "/brightness")
+
+getLedsMaxBrightnessFile :: FilePath -> String
+getLedsMaxBrightnessFile = (++ "/max_brightness")
 
 newtype Percent = Percent Float deriving (Eq, Fractional, Ord, Num)
 
@@ -47,60 +144,58 @@ instance Show Percent where
   show (Percent f) = show (f * 100) ++ "%"
 
 main :: IO ()
-main =
-  getArgs >>= \case
-    ["-get"] -> runGet
+main = (parseArgs <$> getArgs) >>= \case
+  ParseError ShowHelp -> putStrLn usage
+  ParseError (InvalidUsage msg) -> hPutStrLn stderr (msg <> "\n" <> usage) >> exitFailure
+  ParseSuccess args -> runCommand args
 
-    [cmd, s] -> runCommand cmd s
-
-    [help]
-      | help `elem` ["--help", "-h", "-?"] ->
-        putStrLn usage
-
-    _ -> do
-      hPutStrLn stderr usage
-      exitFailure
-
-getCurrentBrightness :: IO Int
-getCurrentBrightness = do
-  s <- readFile ledsBrightnessFile
+getCurrentBrightness :: FilePath -> IO Int
+getCurrentBrightness ledsDir = do
+  s <- readFile $ getLedsBrightnessFile ledsDir
   case readMaybe s of
     Just n -> return n
     Nothing -> error $ "Invalid brightness file value: " ++ s
 
-getMaxBrightness :: IO Int
-getMaxBrightness = do
-  s <- readFile ledsMaxBrightnessFile
+getMaxBrightness :: FilePath -> IO Int
+getMaxBrightness ledsDir = do
+  s <- readFile $ getLedsMaxBrightnessFile ledsDir
   case readMaybe s of
     Just n -> return n
     Nothing -> error $ "Invalid max brightness file value: " ++ s
 
-setBrightness :: Int -> IO ()
-setBrightness n = writeFile ledsBrightnessFile $ show n
+setBrightness :: FilePath -> Int -> IO ()
+setBrightness ledsDir n = writeFile (getLedsBrightnessFile ledsDir) $ show n
 
-runGet :: IO ()
-runGet = do
-  current <- getCurrentBrightness
-  maxVal <- getMaxBrightness
-  print $ mkPercent current maxVal
+runCommand :: Args -> IO ()
+runCommand Args{..} = do
+  ledsDir <- getLedsDir argDevice
+  maxVal <- getMaxBrightness ledsDir
 
-runCommand :: String -> String -> IO ()
-runCommand cmd s = do
-  let perc = parsePercent s
-  current <- getCurrentBrightness
-  maxVal <- getMaxBrightness
-  let currentPerc = mkPercent current maxVal
-  case () of
-    _ | cmd `elem` ["-set", "="] -> do
-          setBrightness $ toBrightness perc maxVal
-          print perc
+  let modify
+        :: Amount
+        -> (forall a. (Ord a, Fractional a) => a -> a)
+        -> (forall a. Num a => a -> a -> a)
+        -> IO ()
+      modify amount limit op = do
+        curVal <- getCurrentBrightness ledsDir
+        let curPercent = mkPercent curVal maxVal
+        case amount of
+          RawAmount n -> setBrightness ledsDir $ curVal `op` n
+          PercentAmount p -> do
+            let newPercent = limit $ curPercent `op` p
+            setBrightness ledsDir $ toBrightness newPercent maxVal
+            print newPercent
 
-      | cmd `elem` ["-inc", "+"] -> do
-          let newPerc = min 1.0 $ currentPerc + perc
-          setBrightness $ toBrightness newPerc maxVal
-          print newPerc
+  case argCommand of
+    Get -> do
+      current <- getCurrentBrightness ledsDir
+      maxVal <- getMaxBrightness ledsDir
+      print $ mkPercent current maxVal
 
-      | cmd `elem` ["-dec", "-"] -> do
-          let newPerc = max 0.0 $ currentPerc - perc
-          setBrightness $ toBrightness newPerc maxVal
-          print newPerc
+    Set amount -> case amount of
+      RawAmount n -> setBrightness ledsDir n
+      PercentAmount p -> setBrightness ledsDir $ toBrightness p maxVal
+
+    Inc amount -> modify amount (min 1.0) (+)
+    Dec amount -> modify amount (max 0.0) (-)
+
